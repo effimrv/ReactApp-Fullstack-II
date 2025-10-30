b9cacf2fc85ebef7aceffc529208c6399503f6c3
Cely <ara.escobar@duocuc.cl>
2025-10-29 23:12:04 -0300

    Cambios de estilos

--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
             <div className="row align-items-center justify-content-center">
               <div className="col-lg-6 d-flex flex-column justify-content-center align-items-center" style={{ height: '100%' }}>
               <h1 className="display-3 fw-bold mb-4 text-white" style={{ letterSpacing: '-0.02em' }}>
-                Eleva tu <span style={{ color: '#e0e7ff' }}>Experiencia</span> Gaming
+                Eleva tu <span style={{ color: '#ffffffff' }}>Experiencia</span> Gaming
               </h1>
                 <p className="lead mb-4 text-white-200 text-center" style={{ fontSize: '1.25rem', maxWidth: '500px' }}>
                 Encuentra los periféricos y accesorios más avanzados para maximizar tu rendimiento en el juego.
